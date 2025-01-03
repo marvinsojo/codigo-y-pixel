6588085c02dbfb618363b38679458a1c32403b02
Marvin Sojo <marvin526@gmail.com>
2025-01-03 19:03:21 -0400

    DELETE: App.jsx y App.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDom from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 import { createHashRouter, RouterProvider } from "react-router-dom";
 
