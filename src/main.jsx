d8083bd5bb4675992c8c9c67faf21e359126192b
Marvin Sojo <marvin526@gmail.com>
2025-01-03 18:23:39 -0400

    ADD: RouterProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,30 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "about",
+    element: <About />,
+  },
+  {
+    path: "/*",
+    element: <NotFound />,
+  },
+]);
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
