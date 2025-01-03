1b9dcfad0749f79271934165a1b0663f9de90723
Marvin Sojo <marvin526@gmail.com>
2025-01-03 19:10:21 -0400

    Prueba Deploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <div className="flex items-center justify-center h-screen w-full">
       <div className="flex flex-col items-center justify-center bg-black bg-opacity-60 p-8 rounded">
         <h1 className="text-6xl m-4 text-white text-shadow">
-          Bienvenidos a Código y Píxel Esto es una prueba
+          Bienvenidos a Código y Píxel
         </h1>
         <div className="flex justify-center sm:justify-start  m-4">
           <Link to="/about">
