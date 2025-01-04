5a35b72d20eade1bb7cae68e6da7f6edad316e61
Marvin Sojo <marvin526@gmail.com>
2025-01-03 21:55:13 -0400

    FIX: About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <div className="flex items-center justify-center h-screen w-full">
     <div className="flex flex-col items-center justify-center bg-black bg-opacity-60 p-8 rounded">
-      <h1 className="text-6xl m-4 text-white text-shadow">Proximamente te vamos a contar más de nosotros</h1>
+      <h1 className="text-6xl m-4 text-white text-shadow">Proximamente te contaremos más de nosotros</h1>
       <Link to="/" className="flex justify-center sm:justify-start  m-4">
         <Button text="Home"/>
       </Link>
